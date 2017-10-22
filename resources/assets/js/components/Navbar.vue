9cc5c503cf7e25c259df0ecdcdcb4b6777e24648
ABCrafty <alexandreblack@hotmail.fr>
2017-10-22 19:00:56 +0200

    premier commit

---
+++ resources/assets/js/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <ul class="navbar">
+        <li class="nav-item">Item</li>
+        <li class="nav-item">Item</li>
+        <li class="nav-item">Item</li>
+        <li class="nav-item">Item</li>
+        <li class="nav-item">Item</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Navbar chargée')
+        }
+    }
+</script>
---
+++ resources/views/homepage/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Hello world</h1>
+
+    <navbar></navbar>
+
+    <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Dropdown button
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">Action</a>
+            <a class="dropdown-item" href="#">Another action</a>
+            <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+    </div>
+
+@endsection
